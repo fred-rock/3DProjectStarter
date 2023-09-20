56a909f130b542522868d8effdfc55c6504075d1
Adam Frederick <adam.paul.frederick@gmail.com>
2023-09-20 16:22:45 -0400

    Cleaned up some unneeded Scenes

--- Assets/Scripts/UI/HUDCanvas.cs
+++ Assets/Scripts/UI/HUDCanvas.cs
@@ -1,13 +1,46 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDCanvas : MonoBehaviour
 {
-    [SerializeField] private AmmoType _ammoType_1;
+    [SerializeField] private BasePlayerWeaponModule _weapon_1;
+    [SerializeField] private Image _weaponIcon_1;
     [SerializeField] private TextMeshProUGUI _ammoText_1;
-    [SerializeField] private AmmoType _ammoType_2;
+
+    [SerializeField] private BasePlayerWeaponModule _weapon_2;
+    [SerializeField] private Image _weaponIcon_2;
     [SerializeField] private TextMeshProUGUI _ammoText_2;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_3;
+    //[SerializeField] private Image _weaponIcon_3;
+    //[SerializeField] private TextMeshProUGUI _ammoText_3;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_4;
+    //[SerializeField] private Image _weaponIcon_4;
+    //[SerializeField] private TextMeshProUGUI _ammoText_4;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_5;
+    //[SerializeField] private Image _weaponIcon_5;
+    //[SerializeField] private TextMeshProUGUI _ammoText_5;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_6;
+    //[SerializeField] private Image _weaponIcon_6;
+    //[SerializeField] private TextMeshProUGUI _ammoText_6;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_7;
+    //[SerializeField] private Image _weaponIcon_7;
+    //[SerializeField] private TextMeshProUGUI _ammoText_7;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_8;
+    //[SerializeField] private Image _weaponIcon_8;
+    //[SerializeField] private TextMeshProUGUI _ammoText_8;
+
+    //[SerializeField] private BasePlayerWeaponModule _weapon_9;
+    //[SerializeField] private Image _weaponIcon_9;
+    //[SerializeField] private TextMeshProUGUI _ammoText_9;
+
     [SerializeField] private TextMeshProUGUI _healthText;
     [SerializeField] private RectTransform _deathScreen;
     [SerializeField] private RectTransform _hurtScreen;
@@ -35,8 +68,15 @@ public class HUDCanvas : MonoBehaviour
             HideDeathScreen();
         }
 
-        _ammoText_1.text = _player.AmmoModule.GetCurrentAmount(_ammoType_1).ToString();
-        _ammoText_2.text = _player.AmmoModule.GetCurrentAmount(_ammoType_2).ToString();
+        _ammoText_1.text = _player.AmmoModule.GetCurrentAmount(_weapon_1.WeaponData.AmmoType).ToString();
+        _ammoText_2.text = _player.AmmoModule.GetCurrentAmount(_weapon_2.WeaponData.AmmoType).ToString();
+        //_ammoText_3.text = _player.AmmoModule.GetCurrentAmount(_weapon_3.WeaponData.AmmoType).ToString();
+        //_ammoText_4.text = _player.AmmoModule.GetCurrentAmount(_weapon_4.WeaponData.AmmoType).ToString();
+        //_ammoText_5.text = _player.AmmoModule.GetCurrentAmount(_weapon_5.WeaponData.AmmoType).ToString();
+        //_ammoText_6.text = _player.AmmoModule.GetCurrentAmount(_weapon_6.WeaponData.AmmoType).ToString();
+        //_ammoText_7.text = _player.AmmoModule.GetCurrentAmount(_weapon_7.WeaponData.AmmoType).ToString();
+        //_ammoText_8.text = _player.AmmoModule.GetCurrentAmount(_weapon_8.WeaponData.AmmoType).ToString();
+        //_ammoText_9.text = _player.AmmoModule.GetCurrentAmount(_weapon_9.WeaponData.AmmoType).ToString();
     }
 
     private void ShowDeathScreen()
